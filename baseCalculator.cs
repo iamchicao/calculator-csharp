8ba952be4ac4c7b7a8f8d76dc7c0596eec3ac057
Francisco Junior <jr-francis2011@hotmail.com>
2022-11-21 22:02:14 -0300

    first commit

---
+++ baseCalculator.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Calculator {
+  public float Number1;
+  public float Number2;
+
+  public Calculator(float number1, float number2) {
+    Number1 = number1;
+    Number2 = number2;
+
+    Operations();
+  }
+
+  public void Operations()
+  {
+     Console.WriteLine($"{Sum(2, 3)}");
+  }
+
+  public float Sum(float number1, float number2)
+  {
+    return number1 + number2;
+  }
+
+
+}
