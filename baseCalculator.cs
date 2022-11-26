fe8e2bd5ea9204da282c750fcd340c691fafa6ad
Francisco Junior <jr-francis2011@hotmail.com>
2022-11-26 00:40:51 -0300

    refactor to solved erros to compile code

--- baseCalculator.cs
+++ baseCalculator.cs
@@ -1,55 +1,29 @@
 using System;
 
 public class Calculator {
-  public float Number1;
-  public float Number2;
-
-  public Calculator(float number1, float number2) {
-    Number1 = number1;
-    Number2 = number2;
-
-    Operations();
+  static void Main() {
+    Console.WriteLine($"{Sum(5, 5)}");
+    Console.WriteLine($"{Subtraction(15, 10)}");
+    Console.WriteLine($"{Multiplication(5, 5)}");
+    Console.WriteLine($"{Division(25, 5)}");
   }
-
-  public void Operations()
-  {
-     Console.WriteLine($"{Sum(2, 3)}");
-     Console.WriteLine($"{Subtraction(2, 3)}");
-     Console.WriteLine($"{Multiplication(2, 3)}");
-     Console.WriteLine($"{Division(2, 3)}");
-  }
-
-  public float Sum(float number1, float number2)
+  
+  static float Sum(float number1, float number2)
   {
     return number1 + number2;
   }
-  
-  public float Subtraction(float number1, float number2)
+  static float Subtraction(float number1, float number2)
   {
     return number1 - number2;
   }
 
-  public float Multiplication(float number1, float number2)
+  static float Multiplication(float number1, float number2)
   {
     return number1 * number2;
   }
 
-  public float Division(float number1, float number2)
+  static float Division(float number1, float number2)
   {
-    return number1 / number;
+    return number1 / number2;
   }
 }
-
-//Example
-/* class Program {
-    static void Main() {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine(Sum(2, 3));
-    }
-    
-    static float Sum(float number1, float number2)
-    {
-      return number1 + number2;
-    }
-}
-*/
